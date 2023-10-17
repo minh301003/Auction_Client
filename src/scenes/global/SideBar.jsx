4377a9e27a935fca2f8bf0aa650d2130827efafd
minh301003 <chunghoangminhtb@gmail.com>
2023-10-17 21:32:39 +0700

    First commit

---
+++ src/scenes/global/SideBar.jsx
@@ -0,0 +1,12 @@
+
+
+
+const SideBar = () => {
+   return (
+    <h1>This is Side Bar</h1>
+   )
+
+};
+
+export default SideBar;
+
---
+++ src/scenes/global/TopBar.jsx
@@ -0,0 +1,10 @@
+const TopBar = () => {
+    return (
+     <h1>This is Top Bar</h1>
+    )
+ 
+ };
+ 
+ export default TopBar;
+ 
+ 
