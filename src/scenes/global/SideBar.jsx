50206ae000856dceb95e7d4a5fe5f3e7840030bc
minh301003 <chunghoangminhtb@gmail.com>
2023-10-18 10:52:12 +0700

    Sidebar + Topbar

--- src/scenes/global/SideBar.jsx
+++ src/scenes/global/SideBar.jsx
@@ -1,29 +1,104 @@
 import { useState } from 'react';
 import { Sidebar, Menu, MenuItem } from 'react-pro-sidebar';
-import { Box, IconButton, Typography, useTheme } from '@mui/material';
+import { Box, IconButton, Typography } from '@mui/material';
 import { Link } from "react-router-dom";
+import MenuIcon from '@mui/icons-material/Menu';
+import HomeIcon from '@mui/icons-material/Home';
+import AddModeratorIcon from '@mui/icons-material/AddModerator';
+const Item = ({ title, to, icon, selected, setSelected }) => {
+  return (
+    <MenuItem
+      active={selected === title}
 
+      onClick={() => setSelected(title)}
+      icon={icon}
+      component={<Link to={to} />}
+    >
+      <Typography color="#8ba1b7">{title}</Typography>
+ 
+    </MenuItem>
+  );
+};
 
-
+const themes = {
+  sidebar: {
+    backgroundColor: "#ffffff",
+    color: "#0098e5",
+  },
+  
+};
 const SideBar = () => {
-    
+    const [isCollapsed, setIsCollapsed] = useState(false);
+    const [selected, setSelected] = useState("AuctionList");
     return (
-        <Sidebar>
-        <Menu
-          menuItemStyles={{
-            button: {
-              [`&.active`]: {
-                backgroundColor: '#13395e',
-                color: '#b6c8d9',
-              },
-            },
-          }}
-        >
-          <MenuItem component={<Link to="/" />}> Danh sách đấu giá</MenuItem>
-          <MenuItem component={<Link to="/createAuction" />}> Đăng bán đấu giá</MenuItem>
+      <Sidebar collapsed={isCollapsed}
+      rootStyles={{
+        backgroundColor: themes.sidebar.backgroundColor,
+        color: themes.sidebar.color,
+
+      }}>
+        <Menu>
+          <MenuItem
+            onClick={() => setIsCollapsed(!isCollapsed)}
+            icon={isCollapsed ? <MenuIcon /> : undefined}
+            style={{
+              margin: "10px 0 20px 0",
+              color: "#0098e5",        
+            }}
+          >
+            {!isCollapsed && (
+              <Box
+                display="flex"
+                justifyContent="space-between"
+                alignItems="center"
+              >
+                <Typography variant="h5" fontWeight="Bold">
+                  Fish Auction
+                </Typography>
+                <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
+                  <MenuIcon />
+                </IconButton>
+              </Box>
+            )}
+          </MenuItem>
+
+          {!isCollapsed && (
+            <Box mb="25px">
+              <Box display="flex" justifyContent="center" alignItems="center">
+                <img
+                  alt="profile-user"
+                  width="100px"
+                  height="100px"
+                  src="https://thumbs.dreamstime.com/b/businessman-icon-image-male-avatar-profile-vector-glasses-beard-hairstyle-179728610.jpg"
+                  style={{ cursor: "pointer", borderRadius: "50%" }}
+                />
+              </Box>
+              <Box textAlign="center" >
+                <Typography variant="h6" color="#8ba1b7" fontWeight="Bold">UserName</Typography>
+              </Box>
+            </Box>
+          )}
+
+          <Box paddingLeft={isCollapsed ? undefined : "10%"}>
+            <Typography color="#8ba1b7" fontWeight="Bold" sx={{ m: "15px 0 5px 20px" }}>Danh mục</Typography>
+            <Item
+              title="Danh sách đấu giá"
+              to="/"
+              icon={<HomeIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Đăng bán đấu giá"
+              to="/createAuction"
+              icon={<AddModeratorIcon />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+          </Box>
         </Menu>
       </Sidebar>
-   );
+    );
 
 };
 
--- src/scenes/global/TopBar.jsx
+++ src/scenes/global/TopBar.jsx
@@ -1,7 +1,24 @@
+import { Box, IconButton, InputBase, Button, Typography } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import LogoutIcon from '@mui/icons-material/Logout';
+
 const TopBar = () => {
+
     return (
-     <h1>This is Top Bar</h1>
-    )
+      <Box style={{ display: "flex", justifyContent: "space-between" }} p={2}>
+        {/* SEARCH BAR */}
+        <Box display="flex" borderRadius="3px" >
+          <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Tìm kiếm" />
+          <IconButton type="button" sx={{ p: 1 }}>
+            <SearchIcon />
+          </IconButton>
+        </Box>
+
+        <Button variant="contained" startIcon={<LogoutIcon />}>
+            <Typography>Đăng xuất</Typography>
+        </Button>
+      </Box>
+    );
  
  };
  
